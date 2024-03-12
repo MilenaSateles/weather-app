7bfbe978a3e33db76d79d88fb3dee3bdf68533f6
Milena Sateles <milenamcsateles@gmail.com>
2024-03-12 15:28:02 -0300

    feat: consumo do serviço criado

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
+//Injeção do serviço criado
 export class WeatherHomeComponent {
+  constructor (private weatherService : WeatherService){}
 
+  getWeatherData(cityName: string): void{
+    this.weatherService.getWeatherData(cityName)
+    .subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => console.log(error)
+    })
+
+  }
 }
